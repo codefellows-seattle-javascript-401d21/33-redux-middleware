a84dd4c4073fc61c88b6f06651bdf7e80a32490c
billodell <billodell5@gmail.com>
2018-02-28 15:21:37 -0800

    indent

--- lab-bill/src/reducers/expense.js
+++ lab-bill/src/reducers/expense.js
@@ -15,18 +15,15 @@ export default (state=initialState, action) => {
           state[payload.categoryId] = state[payload.categoryId].concat([payload])
           return {...state}
         case 'EXPENSE_UPDATE': 
-        let updatedState = {...state}
-        updatedState[payload.categoryId] = updatedState[payload.categoryId].map(ele => ele._id === payload._id ? payload : ele)
-        return updatedState
-        updatedState[payload.categoryId] = updatedState[payload.categoryId].map(ele => ele._id === payload._id ? payload : ele)
-        return updatedState
+          let updatedState = {...state}
+          updatedState[payload.categoryId] = updatedState[payload.categoryId].map(ele => ele._id === payload._id ? payload : ele)
+          return updatedState
         case 'EXPENSE_DESTROY':
-        console.log('state{payload}',state[payload.categoryId]);
-        console.log('payload',payload);
-        let deleteState = {...state}
-        deleteState[payload.categoryId] = deleteState[payload.categoryId].filter(ele => ele._id !== payload._id)
-        return deleteState
+          let deleteState = {...state}
+          deleteState[payload.categoryId] = deleteState[payload.categoryId].filter(ele => ele._id !== payload._id)
+          return deleteState
         case 'EXPENSE_RESET': return initialState
+        
         default: return state
       }
 }
