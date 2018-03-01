196b886fe8f21c9e4f65af71ea9b7c3a02d24099
Kevin Miller <kevin@driftabout.com>
2018-02-28 17:36:59 -0800

    installed redux-devtools-extension

--- lab-kevin/src/lib/store.js
+++ lab-kevin/src/lib/store.js
@@ -1,6 +1,12 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
 import reducers from '../reducers';
+import store_reporter from '../middleware/store-reporter';
 
-let  store = createStore(reducers);
+let  store =  (process.env.NODE_ENV !== 'production') ? 
+  createStore(reducers, 
+    composeWithDevTools(applyMiddleware(store_reporter)
+    )) 
+  : createStore(reducers);
 
 export default store;
