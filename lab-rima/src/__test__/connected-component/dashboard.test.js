765b3c8d4ec6986e41b2ce4b28e6c6517e395caa
Rima <rmhrok@gmail.com>
2018-03-01 06:56:13 +0000

    restructure folders/add another test for dashboard

--- lab-rima/src/__test__/connected-component/dashboard.test.js
+++ lab-rima/src/__test__/connected-component/dashboard.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import createStore from '../lib/store';
+import createStore from '../../lib/store';
 import Adapter from 'enzyme-adapter-react-16';
 import {configure, mount} from 'enzyme';
-import Dashboard from '../components/dashboard/index';
-import App from '../components/app/index';
+import Dashboard from '../../components/dashboard/index';
+import App from '../../components/app/index';
 
 require('jest');
 
@@ -15,11 +15,17 @@ describe('<Dashboard />', function(){
 
   beforeAll(() => {
     this.wrapper = mount(<Provider store={createStore()}><Dashboard /></Provider>);
+    this.wrapper.setProps({categories: [{id: '1', name: 'test', budget: 100}]});
   });
   afterAll(() => this.wrapper.unmount());
 
   test('should render two CategoryForm component', () => {
     expect(this.wrapper.find('CategoryForm').length).toBe(1);
   });
+
+  test('should have an access to state in store', () => {
+    expect(this.wrapper.props().categories[0]).toEqual({id: '1', name: 'test', budget: 100});
+  });
+
 });
 
