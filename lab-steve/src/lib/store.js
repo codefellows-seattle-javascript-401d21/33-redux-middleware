afb3f8dd9db4de6a212c7184b2392081309cac17
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-28 22:49:16 -0800

    Added middleware, reducer validators, and localStorage restore

--- lab-steve/src/lib/store.js
+++ lab-steve/src/lib/store.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 // pulls in combineReducers from '../reducers/index.js'
 import reducer from '../reducers';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxReporter from '../middleware/redux-reporter';
+import crashReporter from '../middleware/crash-reporter';
+import reduxSession from '../middleware/redux-session-middleware';
 
-export default () => createStore(reducer);
+export default () => createStore(reducer, composeWithDevTools(
+  applyMiddleware(reduxReporter, crashReporter, reduxSession)
+));
--- lab-steve/src/reducers/category.js
+++ lab-steve/src/reducers/category.js
@@ -1,5 +1,12 @@
-export default (state=[], action) => {
+let initialState = localStorage['categories'] ? JSON.parse(localStorage['categories']) : [];
+export default (state=initialState, action) => {
   let { type, payload } = action;
+
+  // sgc - Validate correct information was provided
+  if (type.startsWith('CATEGORY')) {
+    if (payload && !payload.name) throw new Error('no name specified for category');
+    if (payload && !payload.budget) throw new Error('no budget specified for category');
+  }
 
   switch (type) {
   case 'CATEGORY_CREATE': return [ ...state, payload ];
