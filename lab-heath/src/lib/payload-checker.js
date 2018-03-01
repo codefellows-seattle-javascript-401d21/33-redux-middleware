c8b0298ca39aa1eaea7752fefe2aec2c7b75765c
heath smith <heathsmith73@gmail.com>
2018-03-01 09:15:43 -0800

    update

--- lab-heath/src/lib/payload-checker.js
+++ lab-heath/src/lib/payload-checker.js
@@ -1,12 +1,12 @@
 
 export const checker = arr => {
   if(!arr) {
-    return 'No Payload detected';
+    throw new Error('No Payload detected');
   }
 
   let {timestamp, title, updating, value, _id} = arr;
   if(!timestamp, !title, !updating, !value, !_id) {
-    return 'Payload is missing a part of the payload';
+    throw new Error('Payload is missing a part of the payload');
   }
 };
 
